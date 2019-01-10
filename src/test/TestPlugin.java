0a73daf738aba75b66ebcb41e1bf4754149765eb
lokha <aleksey.kovalchuk98@gmail.com>
2019-01-10 18:26:48 +0200

    Git init

---
+++ src/test/TestPlugin.java
@@ -0,0 +1,63 @@
+package test;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin {
+
+    private int var1;
+    private String text;
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.getCommand("testreload").setExecutor(this);
+
+        this.reloadConfigParams();
+    }
+
+    private void reloadConfigParams() {
+        FileConfiguration config = this.getConfig();
+
+        var1 = config.getInt("var1");
+        text = config.getString("text");
+
+        this.getLogger().info("Достали из конфига новые значения: " +
+                "var1 = " + var1 + ", " +
+                "text = " + text);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("test.reload")) {
+            sender.sendMessage("§cНет прав.");
+            return false;
+        }
+
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("settext")) {
+                if (args.length > 1) {
+                    String text = args[1];
+                    this.text = text;
+                    this.getConfig().set("text", text);
+                    this.saveConfig();
+                    sender.sendMessage("§aТекст установлен и сохранен.");
+                } else {
+                    sender.sendMessage("§cУкажите текст /testreload settext [текст]");
+                    return false;
+                }
+            } else {
+                sender.sendMessage("§cДоступно только /testreload settext [текст]");
+                return false;
+            }
+        } else {
+            this.reloadConfig();
+            this.reloadConfigParams();
+            sender.sendMessage("§aConfig reloaded.");
+        }
+
+        return true;
+    }
+}
